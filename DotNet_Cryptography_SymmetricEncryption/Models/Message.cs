3b074ae216071d0e5cfe06afde4114f423627d95
Paul <paul.j.millar.1971@gmail.com>
2016-09-29 22:20:15 +0100

    Tidy up

--- DotNet_Cryptography_SymmetricEncryption/Models/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotNet_Cryptography_SymmetricEncryption.Models
-{
-    public class Message
-    {
-        public string PlainText { get; set; }
-        public string IV { get; set; }
-        public string Encrypted { get; set; }
-    }
-}
